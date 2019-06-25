61316defc06b5d75d7518d719241b056f7a2f791
Cameron Welter <camjwelt@gmail.com>
2019-06-25 13:04:04 +0000

    make patterns even more betterererer

--- res/patterns.js
+++ res/patterns.js
@@ -1,4 +1,4 @@
 module.exports = {
-  kebabCasePattern: /^[a-z][a-z0-9]{1,}(-[a-z][a-z0-9]{1,})*$/u,
-  pascalCasePattern: /^([A-Z][a-z0-9]{1,})+$/u,
+  kebabCasePattern: /^[a-z][a-z0-9]+(-[a-z0-9]{2,})*$/u,
+  pascalCasePattern: /^([A-Z][a-z0-9]+)+$/u,
 }
