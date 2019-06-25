0af5f870f44ea21bfd46d611daa276e6e3ef0665
Cameron Welter <camjwelt@gmail.com>
2019-06-25 02:38:16 +0000

    add lint rules missing from not having babel.

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,42 @@ module.exports = {
     '@fuelrats/eslint-config/rules/style',
     '@fuelrats/eslint-config/rules/variables',
   ],
+  rules: {
+    'new-cap': ['error', {
+      newIsCap: true,
+      capIsNew: true,
+      newIsCapExceptions: [],
+      capIsNewExceptions: [
+        'GET', // For HTTP methods
+        'POST',
+        'PUT',
+        'DELETE',
+        'Stripe', // For Stripe lib (which exists as window.Stripe)
+      ],
+      properties: true,
+    }],
+
+    'no-invalid-this': ['error'],
+
+    'object-curly-spacing': ['error', 'always'],
+
+    quotes: ['error', 'single', {
+      avoidEscape: true,
+      allowTemplateLiterals: false,
+    }],
+
+    semi: ['error', 'never', {
+      beforeStatementContinuationChars: 'always',
+    }],
+
+    'no-unused-expressions': ['error', {
+      allowShortCircuit: false,
+      allowTaggedTemplates: false,
+      allowTernary: false,
+    }],
+
+    'valid-typeof': ['error'],
+  },
   parserOptions: {
     codeFrame: true,
     ecmaVersion: 2018,
