1b2b7f66cd403899fd21fffe424e4307262fc74f
Cameron Welter <camjwelt@gmail.com>
2020-04-28 17:12:07 +0000

    regex kinda sucks.

--- res/patterns.js
+++ res/patterns.js
@@ -1,5 +1,5 @@
 module.exports = {
   kebabCasePattern: /^[a-z][a-z0-9]*(-[a-z0-9]+)*$/u,
   pascalCasePattern: /^([A-Z][a-z0-9]+)+$/u,
-  camelCasePattern: /^[a-z][A-z0-9]{2,}$/u,
+  camelCasePattern: /^[a-z][A-Za-z0-9]{2,}$/u,
 }
